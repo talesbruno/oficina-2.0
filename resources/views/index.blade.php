820eca35080828974177e4813181e39d9a744ade
Tales Cardoso <105746645+talesbruno@users.noreply.github.com>
2023-06-11 15:36:05 -0300

    styles

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,13 @@
 @extends('layouts.app')
  
 @section('header')
-<nav class="navbar navbar-light bg-light">
-    <a class="navbar-brand" href="#">
-        Listagem de agendamentos
-    </a>
+<nav class="navbar bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand">Oficina-2.0</a>
+      <form class="d-flex" role="search">
+        <a class="icon-link " href="{{route('budgets.create')}}">Adicionar um novo orçamento<i class="bi bi-plus-circle-fill"></i></a>
+      </form>
+    </div>
   </nav>
 @endsection
 
@@ -18,34 +21,27 @@
 <h2>Todos os Orçamentos</h2>
 @endif
 </div>
+
 <div class="container">
-
-    <div class="mb-3">
-        <a class="icon-link " href="{{route('budgets.create')}}">Adicionar um novo orçamento</i></a>
-    </div>
-
-
-<form method="GET" action="{{ route('budgets.index') }}">
-    <div class="form-group">
-        <div class="mb-3">
-            <h6>Filtrar por: </h6>
+    <form class="form-inline" method="GET" action="{{ route('budgets.index') }}">
+        <div class="row">
+        <div class="col">
+            <label for="startDate">Data inicial (YYYY-MM-DD)</label>
+            <input class="form-control" type="datetime-local" name="startDate">
         </div>
-        <div class="mb-3">
-    <label for="startDate">Data inicial (YYYY-MM-DD)</label>
-    <input class="form-control" type="datetime-local" name="startDate">
-</div>
-<div class="mb-3">
-    <label for="endDate">Data final (YYYY-MM-DD)</label>
-    <input class="form-control" type="datetime-local" name="endDate">
-</div>
-<div class="mb-3">
-    <label for="">Ou</label>
-    <input class="form-control" type="text" name="filter" placeholder="Filtro por cliente ou vendedor">
-</div>
-    <button class="btn btn-primary" type="submit">Filtrar</button>
+        <div class="col">
+            <label for="endDate">Data final (YYYY-MM-DD)</label>
+            <input class="form-control" type="datetime-local" name="endDate">
+        </div>
+        <div class="col">
+            <label for="filter">Ou</label>
+            <input class="form-control" type="text" name="filter" placeholder="Filtro por cliente ou vendedor">
+        </div>
+        <div class="col">
+        <button class="btn btn-primary" type="submit">Filtrar</button>
     </div>
-</form>
-
+    </div>
+      </form>
 <table class="table table-striped">
     <thead>
         <th scope="col">Cliente</th>
@@ -63,12 +59,12 @@
                 <td>{{$budget->date}}</td>
                 <td>{{$budget->seller}}</td>
                 <td>
-                    <a href="{{route('budgets.show', $budget->id)}}">ver mais</a>
-                    <a href="{{route('budgets.edit', $budget->id)}}">editar</a>
+                    <a href="{{route('budgets.show', $budget->id)}}"><i class="bi bi-eye-fill"></i></a>
+                    <a href="{{route('budgets.edit', $budget->id)}}"><i class="bi bi-pencil-square"></i></a>
                     <form action="{{route('budgets.destroy', $budget->id)}}" method="POST">
                         @csrf
                         @method('DELETE')
-                        <button type="submit" class="btn"><i class="fa-solid fa-trash-can"></i>deletar</button>
+                        <button type="submit" class="btn"><i class="bi bi-trash3-fill"></i></button>
                     </form>
                 </td>
             </tr>
